41b2f360271a80bd300e107446214b7ad3059ffe
Edward Tanguay (windows) <edward@tanguay.info>
2022-12-07 09:40:13 +0100

    finished

--- src/pages/PageJobs.tsx
+++ src/pages/PageJobs.tsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
 import { AppContext } from '../appContext';
-import { Job } from '../types';
+import { Job, Skill } from '../types';
 
 export const PageJobs = () => {
 	const { jobs } = useContext(AppContext);
@@ -9,7 +9,7 @@ export const PageJobs = () => {
 		<div className="page pageJobs">
 			<div className="jobs">
 				<h2>There are {jobs.length} jobs:</h2>
-				{jobs.map((job) => {
+				{jobs.map((job: Job) => {
 					return (
 						<div className="job" key={job.id}>
 							<div className="title">
@@ -18,9 +18,30 @@ export const PageJobs = () => {
 								</a>
 							</div>
 							<div className="company">{job.company}</div>
+							<div className="todo">NEXT TASK: {job.todo}</div>
 							<div className="description">{job.description}</div>
-							<div className="skills">{job.skillList}</div>
-							<div className="todo">{job.todo}</div>
+							<div className="skills">
+								{job.skills.map((skill: Skill) => {
+									return (
+										<>
+											{skill.name ? (
+												<div className="skill found">
+													<div className="name">
+														<a href={skill.url} target="_blank">{skill.name}</a> - {skill.description}
+													</div>
+												</div>
+											) : (
+												<div className="skill missing">
+													<div className="name">
+														
+														<a href={`https://www.google.com/search?q=${skill.idCode}+web+development`} target="_blank">{skill.idCode}</a> - ADD TO BACKEND: \src\data\skillInfos.json
+													</div>
+												</div>
+											)}
+										</>
+									);
+								})}
+							</div>
 						</div>
 					);
 				})}
--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,17 @@
 export type Job = {
-    id: number;
-    title: string;
-    company: string;
-    description: string;
-    url: string;
-    skillList: string;
-    todo: string;
-};
+	id: number;
+	title: string;
+	company: string;
+	url: string;
+	description: string;
+	skillList: string;
+	skills: Skill[];
+	todo: string;
+}
+
+export type Skill = {
+	idCode: string;
+	name: string;
+	url: string;
+	description: string;
+}
