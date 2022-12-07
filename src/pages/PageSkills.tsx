fe103b2f3a37ed4aa946678125bbfe51836fa8b0
Edward Tanguay (windows) <edward@tanguay.info>
2022-12-07 18:20:16 +0100

    before open switch

--- src/pages/PageSkills.tsx
+++ src/pages/PageSkills.tsx
@@ -1,7 +1,41 @@
+import { useContext } from 'react';
+import { AppContext } from '../appContext';
+
 export const PageSkills = () => {
+	const { totaledSkills } = useContext(AppContext);
 	return (
 		<div className="page pageSkills">
-			<p>This is the Skills page.</p>
+			<h2>Skills</h2>
+			<div className="totaledSkills">
+				{totaledSkills.map((totaledSkill, i) => {
+					return (
+						<>
+							{totaledSkill.skill.name ? (
+								<div className="totaledSkill found">
+									<div className="name">
+										<span className="total">
+											{totaledSkill.total}x
+										</span>{' '}
+										{totaledSkill.skill.name}
+										{totaledSkill.isOpen && (
+											<div className="openArea">nnn</div>
+										)}
+									</div>
+								</div>
+							) : (
+								<div className="totaledSkill missing">
+									<div className="name">
+										<span className="total">
+											{totaledSkill.total}x
+										</span>{' '}
+										{totaledSkill.skill.idCode}{' '}
+									</div>
+								</div>
+							)}
+						</>
+					);
+				})}
+			</div>
 		</div>
 	);
 };
